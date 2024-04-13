e495a3dfd67efba53f8c9f1eaa4a396d127a3bb9
CrowsVeldt <zechy.mahler@gmail.com>
2024-04-13 17:08:31 -0600

    feat: add getData/getNextDate/getNextPage

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const axios = require("axios");
+const fs = require("node:fs/promises");
 
-const getData = async () => {
+const getData = async (date) => {
   const data = await axios.get(
-    "http://www.girlgeniusonline.com/comic.php?date=20021104"
+    `http://www.girlgeniusonline.com/comic.php?date=${date != null ? date : "20021104"}`
   );
-  console.log(data);
   return data;
 };
 
-getData();
+const getNextDate = async (res) => {
+  const { data } = await res;
+  const nextDate = data.indexOf("topnext");
+  const dateString = data.substring(nextDate - 14, nextDate - 6);
+  return dateString;
+};
 
-// div id=topnav, for dates
+const getNextPage = async (date) => {
+  const nextPageDate = await getNextDate(getData(date))
+  const nextPage = await getData(nextPageDate)
+  console.log(nextPage)
+  return nextPage;
+};
+
+getNextPage("20021106")
+
 
 // option value='20021104', for titles/volumes
