dd521384736e0910d4c6dbdc9a4f4980dec041df
CrowsVeldt <zechy.mahler@gmail.com>
2024-04-13 22:12:37 -0600

    dev: finish recordDates, remove getNextPage

--- index.js
+++ index.js
@@ -3,30 +3,31 @@ const fs = require("node:fs/promises");
 
 const getData = async (date) => {
   const data = await axios.get(
-    `http://www.girlgeniusonline.com/comic.php?date=${date != null ? date : "20021104"}`
+    `http://www.girlgeniusonline.com/comic.php?date=${
+      date != null ? date : "20021104"
+    }`
   );
   return data;
 };
 
-const getNextDate = async (res) => {
-  const { data } = await res;
+const getNextDate = async (date) => {
+  const { data } = await getData(date);
   const nextDate = data.indexOf("topnext");
   const dateString = data.substring(nextDate - 14, nextDate - 6);
   return dateString;
 };
 
-const getNextPage = async (date) => {
-  const nextPageDate = await getNextDate(getData(date))
-  const nextPage = await getData(nextPageDate)
-  return nextPage;
+const recordDates = async () => {
+  const dates = ["20021104"]; // if date list is present, get last date
+  const regex = new RegExp(/[\d]{8}/);
+  while (regex.test(await getNextDate(dates[dates.length - 1]))) {
+    const nextDate = await getNextDate(dates[dates.length - 1]);
+    dates.push(nextDate);
+    console.log(dates);
+  }
+  return dates;
 };
 
-const recordDates = async () => {
-    const firstDate = "20021104"
-    const nextDate = getNextDate(firstDate)
-}
-
-// getNextPage("20021106")
-
+recordDates()
 
 // option value='20021104', for titles/volumes
