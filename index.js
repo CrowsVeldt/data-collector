60787275c05b7d188d9d485f8375b302c039a4d9
CrowsVeldt <zechy.mahler@gmail.com>
2024-04-14 09:16:07 -0600

    feat: add getTitles, parseTitles, collectVolumes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 const fs = require("node:fs/promises");
 
-const getData = async (date) => {
+const getPage = async (date) => {
   const data = await axios.get(
     `http://www.girlgeniusonline.com/comic.php?date=${
       date != null ? date : "20021104"
@@ -10,14 +10,16 @@ const getData = async (date) => {
   return data;
 };
 
+// DATE COLLECTION FUNCTIONS
+
 const getNextDate = async (date) => {
-  const { data } = await getData(date);
+  const { data } = await getPage(date);
   const nextDate = data.indexOf("topnext");
   const dateString = data.substring(nextDate - 14, nextDate - 6);
   return dateString;
 };
 
-const recordDates = async () => {
+const fetchDates = async () => {
   const dates = ["20021104"]; // if date list is present, get last date
   const regex = new RegExp(/[\d]{8}/);
   while (regex.test(await getNextDate(dates[dates.length - 1]))) {
@@ -28,6 +30,43 @@ const recordDates = async () => {
   return dates;
 };
 
-recordDates()
+// TITLE COLLECTION FUNCTIONS
 
-// option value='20021104', for titles/volumes
+const getTitles = async () => {
+  const res = await getPage();
+  const startIndex = res.data.indexOf("<option value='20021104'>");
+  const endIndex = res.data.indexOf("---Jump to a Scene---<");
+  const titles = res.data.substring(startIndex, endIndex);
+  return titles;
+};
+
+const parseTitles = async () => {
+  const res = await getTitles();
+  const regex = new RegExp(/^([a-zA-Z0-9>'():.\-\s]*)(<\/option>)/g);
+  const list = res.split("<option value='");
+  return list.map((item) => {
+    //console.log(item)
+    const date = item.substring(0, 8);
+    const titleReg = item.match(regex);
+    const matchedTitle = titleReg != null ? titleReg[0] : "";
+   // console.log(matchedTitle)
+    const titleStartIndex = matchedTitle.indexOf(">");
+    const titleEndIndex = matchedTitle.indexOf("</option>");
+    const title = matchedTitle.substring(titleStartIndex + 1, titleEndIndex);
+    return { date: date, title: title };
+  });
+};
+
+const collectVolumes = async () => {
+  const titles = await parseTitles();
+  const volumeStarts = titles.filter(
+    (item) =>
+      (item.title.includes("First Page"))
+  );
+   console.log(volumeStarts);
+   return volumeStarts
+};
+collectVolumes();
+
+// PageType = {pageNumber: "number", date: "date", title: "title", volumeNumber: "number"}
+// collect volume into {volumeStart: "date", volumeNumber: "number", pages: PageType[]}
