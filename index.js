e621f9a1c265bbe93cce8e7129f569df07c02004
CrowsVeldt <zechy.mahler@gmail.com>
2024-04-17 20:29:30 -0600

    feat: add express app, create in index.js

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-const {fetchDates} = require("./src/dates");
-const { parseTitles } = require("./src/titles");
-const { collectVolumes } = require("./src/volumes");
+const bodyParser = require("body-parser");
+const express = require("express");
+const pages = require("./lists/pageList.json")
+const volumes = require("./lists/volumeList.json")
+// const {fetchDates} = require("./src/dates");
+// const { parseTitles } = require("./src/titles");
+// const { collectVolumes } = require("./src/volumes");
+// collectVolumes()
 
-collectVolumes()
+const app = express();
+app.use(bodyParser.json());
+
+app.get("/", (req, res) =>{
+    res.send({pages, volumes})
+})
+
+app.listen(3123, () => {
+  console.log("listening at port 3123");
+});
