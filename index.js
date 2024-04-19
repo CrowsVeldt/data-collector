332179ab923f90aa9fc5ea6d1e801ae3a9a59a32
CrowsVeldt <zechy.mahler@gmail.com>
2024-04-19 09:03:01 -0600

    dev: update /check

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const bodyParser = require("body-parser");
 const express = require("express");
-const pages = require("./lists/pageList.json")
-const volumes = require("./lists/volumeList.json")
+const pages = require("./lists/pageList.json");
+const volumes = require("./lists/volumeList.json");
 // const {fetchDates} = require("./src/dates");
 // const { parseTitles } = require("./src/titles");
 // const { collectVolumes } = require("./src/volumes");
@@ -10,14 +10,15 @@ const volumes = require("./lists/volumeList.json")
 const app = express();
 app.use(bodyParser.json());
 
-app.get("/", (req, res) =>{
-    res.send({pages, volumes})
-})
+app.get("/", (req, res) => {
+  res.send({ pages, volumes });
+});
 
 app.get("/check", (req, res) => {
-    const {date} = req.params
-    console.log(date)
-})
+  const { date } = req.query;
+  console.log(date);
+  res.send({ data: "date" });
+});
 
 app.listen(3123, () => {
   console.log("listening at port 3123");
